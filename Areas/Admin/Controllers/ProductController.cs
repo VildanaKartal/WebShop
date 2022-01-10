0f5d242acbc1150f4959596d96e4ae4f57ca86a9
vk752 <vk752@LAPTOP-GVQL2UOC>
2022-01-10 09:04:17 +0100

    Add project files.

---
+++ Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WebShop.Data;
+using WebShop.Models;
+
+namespace WebShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext _db;
+        [Obsolete]
+        private IHostingEnvironment _he;
+        [Obsolete]
+        public ProductController(ApplicationDbContext db, IHostingEnvironment he)
+        {
+            _db = db;
+            _he = he;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.Products.Include(c => c.ProductTypes).ToList());
+        }
+        //Get Create method
+        public IActionResult Create()
+        {
+            ViewData["productTypeId"] = new SelectList(_db.ProductTypes.ToList(), "Id", "ProductType");
+
+            return View();
+
+        }
+        //Post Create method
+        [HttpPost]
+        [Obsolete]
+        public async Task<IActionResult> Create(Products products, IFormFile image)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if (image != null)
+                {
+                    var name = Path.Combine(_he.WebRootPath + "/Images", Path.GetFileName(image.FileName));
+                    await image.CopyToAsync(new FileStream(name, FileMode.Create));
+                    products.Image = "Images/" + image.FileName;
+                }
+                if (image == null)
+                {
+                    products.Image = "Images/noimage.jpg";
+                }
+
+                _db.Products.Add(products);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(products);
+        }
+        //Get Edit Action Method
+        [HttpPost]
+        public ActionResult Edit(int? id)
+        {
+            ViewData["productTypeId"] = new SelectList(_db.ProductTypes.ToList(), "Id", "ProductType");
+            if (id == null)
+            {
+
+                return NotFound();
+            }
+            var product = _db.Products.Include(c => c.ProductTypes).FirstOrDefault(c => c.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+
+
+        //POST EDIT METHOD
+        //        [HttpPost]
+        //        public async Task<IActionResult> Edit(Products products, IFormFile image);
+        //                {
+
+        //                    if (ModelState.IsValid)
+        //                    {
+        //                        if (image != null)
+        //                        {
+        //                            var name = Path.Combine(_he.WebRootPath + "/Images", Path.GetFileName(image.FileName));
+        //                   await image.CopyToAsync(new FileStream(name, FileMode.Create));
+        //                            products.Image = "Images/" + image.FileName;
+        //                        }
+        //                        if (image == null)
+        //                        {
+        //                            products.Image = "Images/noimage.jpg";
+        //                        }
+
+        //              _db.Products.Update(products);
+        //              await _db.SaveChangesAsync();
+        //              return RedirectToAction(nameof(Index));
+        //                    }
+        //                    return View(products);
+        //                }
+
+
+        // }
+        //// GET DETAILS METHOD
+        //public ActionResult Details( int id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+
+        //    }
+        //    var product = _db.Products.Include(c => c.ProductTypes).FirstOrDefault(c => c.Id = id);
+
+        //    if (product==null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View();
+
+        // }
+
+
+        //GET  DELETE METHOD
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+
+        {
+            if (id==null)
+            {
+
+                return NotFound();
+            }
+            var product = _db.Products.Include(c => c.ProductTypes).Where(c => c.Id == id).FirstOrDefault();
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+    }
+
+}
---
+++ Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebShop.Data;
+using WebShop.Models;
+
+namespace WebShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        private ApplicationDbContext _db;
+            public ProductTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+        public IActionResult Index()
+        {
+            var data = _db.ProductTypes.ToList();
+            return View(_db.ProductTypes.ToList());
+        }
+        //create Get action metod
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Create Post action metod
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductTypes productTypes)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ProductTypes.Add(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(actionName: nameof(Index));
+            }
+            return View(productTypes);
+        }
+
+        //create Edit action metod
+        public ActionResult Edit(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+
+            }
+            var productType = _db.ProductTypes.Find(id);
+           if (productType ==null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+        //Create Edit action metod
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(ProductTypes productTypes)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ProductTypes.Update(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(actionName: nameof(Index));
+            }
+            return View(productTypes);
+        }
+
+        //create Details action metod
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+
+            }
+            var productType = _db.ProductTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+        //Create Post action metod
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Details(ProductTypes productTypes)
+        {
+            
+                return RedirectToAction(actionName: nameof(Index));
+        
+        }
+
+        //create Delete action metod
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+
+            }
+            var productType = _db.ProductTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+        //Create Delete action metod
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int? id,ProductTypes productTypes)
+        { if(id== null)
+            {
+                return NotFound();
+            }
+
+        if( id!=productTypes.Id)
+            {
+                return NotFound();
+                var producTypes = _db.ProductTypes.Find(id);
+                if(productTypes==null)
+                {
+                    return NotFound();
+                }
+            }
+            if (ModelState.IsValid)
+            {
+                _db.ProductTypes.Remove(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(actionName: nameof(Index));
+            }
+            return View(productTypes);
+        }
+
+    }
+
+}
